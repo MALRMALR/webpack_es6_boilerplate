b8a05b6c1186c6a16bac6b18e82c0d35566cf36f
@MALRMALR <michaelalross@gmail.com>
2020-07-08 03:25:20 -0700

    removed jquery from app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-import jquery from 'jquery';
 import { uniq } from 'lodash';
 
 const malr = 'hi michael';
 console.log(malr);
+
+const arr = [1, 2, 2, 54, 65, 76, 76, 395, 505, 505, 3059];
+console.log(uniq(arr));
